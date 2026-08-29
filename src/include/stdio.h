a7c097a9e6d7d25eb1429ce05b788cf166da9647
littlefly365 <c081526@hotmail.com>
2026-08-29 10:46:44 -0500

    mini-printf: add a basic implementation of printf and eprintf

--- src/include/stdio.h
+++ src/include/stdio.h
@@ -1,6 +1,12 @@
 #ifndef _STDIO_H
 #define _STDIO_H	1
 
+#include <stdarg.h>
+
 #define	BUFSIZ	1024
+
+/* see mini-printf/ for the implementations */
+int printf(const char *fmt, ...);
+int eprintf(const char *fmt, ...);
 
 #endif
