09158a446ac3716e8a80aaf57006b6de07c25d3a
littlefly365 <c081526@hotmail.com>
2026-09-01 08:43:04 -0500

    include: replace struct _locale with void* in inttypes.h

--- src/include/locale.h
+++ src/include/locale.h
@@ -1,6 +1,10 @@
 #ifndef _LOCALE_H
 #define _LOCALE_H	1
 
+#if (_POSIX_C_SOURCE - 0) >= 200809L || defined(_NETBSD_SOURCE)
+#   ifndef __LOCALE_T_DECLARED
 typedef void* locale_t;
+#   define __LOCALE_T_DECLARED
+#   endif
 
 #endif
