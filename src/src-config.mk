7217a17c59a70d38f2e0c5b1954b8b87222dbcc2
littlefly365 <c081526@hotmail.com>
2026-09-19 15:41:11 -0500

    replace abs and div functions in C with functions in assembly

--- src/src-config.mk
+++ src/src-config.mk
@@ -1,14 +1,13 @@
-SUBDIRS=string ctype linux gen stdlib stdio compat locale citrus citrus-modules iconv \
-	gdtoa termios regex thread-sub db time arch/amd64/gen cdb nls
+SUBDIRS_C_SOURCE=string ctype linux gen stdlib stdio compat locale citrus citrus-modules iconv \
+		gdtoa termios regex thread-sub db time arch/amd64/gen cdb nls
+SUBDIRS_S_SOURCE=gen string stdlib
 
-ALL_C_SRCS := $(foreach dir,$(SUBDIRS),$(wildcard $(dir)/*.c)) $(CURDIR)/csu/libc_start.c
+ALL_C_SRCS := $(foreach dir,$(SUBDIRS_C_SOURCE),$(wildcard $(dir)/*.c)) $(CURDIR)/csu/libc_start.c
+ALL_S_SRCS := $(foreach dir,$(SUBDIRS_S_SOURCE),$(wildcard arch/amd64/$(dir)/*.S))
 
 GENERAL_INCLUDES := $(wildcard include/*.h)
 SYSTEM_INCLUDES := $(wildcard include/sys/*.h)
 MACHINE_INCLUDES := $(wildcard arch/$(MACHINE)/machine/*.h)
-
-ARCH_S := $(wildcard arch/amd64/gen/*.S) $(wildcard arch/amd64/string/*.S)
-ALL_S_SRCS := $(ARCH_S)
 
 C_OBJS := $(ALL_C_SRCS:%.c=%.o)
 S_OBJS := $(ALL_S_SRCS:%.S=%.o)
